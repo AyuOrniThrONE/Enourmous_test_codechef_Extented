6826358d5810bea3fc3db1c9f897a5244dd53075
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2021-09-11 20:21:32 +0530

    Added another condition

--- enourmousinputtest.c
+++ enourmousinputtest.c
@@ -2,7 +2,9 @@
 int main()
 {
     int n, k, c = 0;
+    int primes = 0;
     scanf("%d %d", &n, &k);
+
     for (int i = 0; i < n; i++)
     {
         int t, div = 0;
@@ -12,8 +14,19 @@ int main()
         {
             c++;
         }
+        int q = t / 2;
+        
+        if (t % 2 != 0 || t%3!=0 || t%5!=0 || t%7!=0 || t%11!=0)
+        {
+            primes++;
+        }
+        else if (t <= 2 && t > 0)
+        {
+            primes++;
+        }
     }
-    printf("%d\n", c);
+    printf("Number of input divisible by %d is: %d\n", k, c);
+    printf("Number of input that are primes is: %d\n", primes - 1);
 
     return 0;
 }
